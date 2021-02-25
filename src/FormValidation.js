82e81f94327f2a8f5d966c1d53032acd66e08e32
Himanshu Gautam <himanshugautam9787@gmail.com>
2021-02-25 14:51:32 +0530

    Optimize my code ex-22 line to 2 line of onchange event witht the help of name field in all the input field and handleAll event (event name given by me) syntax- this.setState({[event.target.name]:event.target.value})

--- src/FormValidation.js
+++ src/FormValidation.js
@@ -6,7 +6,7 @@ class FormValidation extends Component {
 constructor(props){
   super()
   this.state={ fullname:"Myname",
-               eamil   :"abc@.com",
+               email   :"abc@.com",
                phone   :"9958055434",
                message :"type message here"   
         
@@ -15,25 +15,30 @@ constructor(props){
 //  we have to use onChange in input field for getting rid of read only mode
 //  everything that we reaceive in react is in form of object therefore to change the value of value field we have to use event.target.value
 
-handleName=(event)=>{
-    this.setState({fullname:event.target.value})
-    console.log(event.target.value);
-}
+// handleName=(event)=>{
+//     this.setState({fullname:event.target.value})
+//     console.log(event.target.value);
+// }
 
-handleEmail=(event)=>{
-    this.setState({eamil:event.target.value})
-}
+// handleEmail=(event)=>{
+//     this.setState({eamil:event.target.value})
+// }
 
-handlePhone=(event)=>{
-    this.setState({phone:event.target.value})
-}
+// handlePhone=(event)=>{
+//     this.setState({phone:event.target.value})
+// }
 
-handleMessage=(event)=>{
-    this.setState({message:event.target.value})
+// handleMessage=(event)=>{
+//     this.setState({message:event.target.value})
+// }
+
+handleAll=(event)=>{
+  this.setState({[event.target.name]:event.target.value})
 }
 
 handlesubmit=(event)=>{
-    alert(`my name is ${this.state.fullname}`)
+    // alert(`my name is ${this.state.fullname}`)
+    alert(JSON.stringify(this.state));
     event.preventDefault();
     // event.preventDefault() stop the page to redirect on new page after sending data
 }
@@ -42,17 +47,18 @@ handlesubmit=(event)=>{
             <div>
                 {/* handlesubmit for sending data to server */}
               <form onSubmit ={this.handlesubmit}>
-                <label>Fullaname</label><br/>
-                <input type="text" value={this.state.fullname} onChange={this.handleName}/> <br/>
+                <label>Fullaname</label><br/> 
+                <input type="text" name="fullname" value={this.state.fullname}  onChange={this.handleAll}/> <br/>                                
+                {/* <input type="text" value={this.state.fullname} name="fullname" onChange={this.handleName}/> <br/> */}
                 
                 <label>Email</label><br/>
-                <input type="email" value={this.state.eamil} onChange={this.handleEmail}/> <br/>
+                <input type="email"  name="email" value={this.state.email} onChange={this.handleAll}/> <br/>
 
                 <label>Mobile</label><br/>
-                <input type="number" value={this.state.phone} onChange={this.handlePhone}/> <br/>
+                <input type="number" name="phone" value={this.state.phone}  onChange={this.handleAll}/> <br/>
 
                 <label>Message </label><br/>
-                <textarea value={this.state.message} onChange={this.handleMessage}/> <br/>
+                <textarea value={this.state.message} name="message"  onChange={this.handleAll}/> <br/>
 
                 <input type="submit" value="send"/> 
                 {/* if type ="submit" it'll redirect to new page(property of js) */}
