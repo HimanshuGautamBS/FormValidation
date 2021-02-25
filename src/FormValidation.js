b6a53c0465196da7c3c44f039543000377cdd19c
Himanshu Gautam <himanshugautam9787@gmail.com>
2021-02-25 13:42:05 +0530

    handle event like handleSubmit for form and onChange for set the state and value of different field for data submission

--- src/FormValidation.js
+++ src/FormValidation.js
@@ -19,10 +19,29 @@ handleName=(event)=>{
     this.setState({fullname:event.target.value})
     console.log(event.target.value);
 }
+
+handleEmail=(event)=>{
+    this.setState({eamil:event.target.value})
+}
+
+handlePhone=(event)=>{
+    this.setState({phone:event.target.value})
+}
+
+handleMessage=(event)=>{
+    this.setState({message:event.target.value})
+}
+
+handlesubmit=(event)=>{
+    alert(`my name is ${this.state.fullname}`)
+    event.preventDefault();
+    // event.preventDefault() stop the page to redirect on new page after sending data
+}
     render() {
         return (
             <div>
-              <form>
+                {/* handlesubmit for sending data to server */}
+              <form onSubmit ={this.handlesubmit}>
                 <label>Fullaname</label><br/>
                 <input type="text" value={this.state.fullname} onChange={this.handleName}/> <br/>
                 
@@ -35,7 +54,8 @@ handleName=(event)=>{
                 <label>Message </label><br/>
                 <textarea value={this.state.message} onChange={this.handleMessage}/> <br/>
 
-                <input type="submit" value="send"/>
+                <input type="submit" value="send"/> 
+                {/* if type ="submit" it'll redirect to new page(property of js) */}
               </form>
             </div>
         )
